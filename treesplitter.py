dd38b0b39f89d0fa2e431b82fe0aed70281369b6
Frank Hui <huifrank@live.com>
2020-01-10 18:30:34 -0800

    add trim node and delDescendants

--- treesplitter.py
+++ treesplitter.py
@@ -13,3 +13,16 @@ for node in tree.traverse("preorder"):
     print(temp)
 
 print(tree)
+
+def trimNode(node: Tree, prevLength=0):
+    tree = node.copy()
+    for child in tree.get_children():
+        if child.dist > (1.4 + prevLength):
+            delDescendants(child)
+            child.delete()
+        else:
+            trimNode(child, prevLength=child.dist)
+
+def delDescendants(node:Tree):
+    for desc in node.get_descendants():
+        desc.delete()
