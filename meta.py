5dbd6600f3cc2ef2112e397e4a19507ea9f828c2
Frank Hui <huifrank@live.com>
2020-02-08 11:25:52 -0800

    Add verbosity to meta

--- meta.py
+++ meta.py
@@ -3,7 +3,7 @@ import pandas as pd
 meta = pd.read_csv("nextstrain_flu_seasonal_h3n2_ha_12y_metadata.tsv", sep='\t')
 print("Tego[meta.py]: Metadata loaded.")
 headers = list(meta.columns.values.tolist())
-def get(strain: str, attributes: list = []):
+def get(strain: str, attributes: list = [], verbose = 9):
     data = {}
     if len(attributes) == 0:
         attributes = headers.copy()
@@ -15,5 +15,6 @@ def get(strain: str, attributes: list = []):
             try:
                 data[attribute] = meta.loc[meta["Strain"] == strain][attribute].tolist()[0]
             except IndexError:
-                print("Did not find " + strain)
+                if verbose == 1:
+                    print("Did not find " + strain)
     return data
