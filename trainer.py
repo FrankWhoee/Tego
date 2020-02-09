60e36e4047fbdb4a1e496427a871d96cd6aa5fb1
Frank Hui <huifrank@live.com>
2020-02-09 11:19:01 -0800

    Revert to model 5 struct

--- trainer.py
+++ trainer.py
@@ -32,6 +32,24 @@ X_train, X_test, \
 E_train, E_test, \
 y_train, y_test = train_test_split(A, X, E, y, test_size=0.1)
 
+A, X, E, y = getData()
+print("Data acquired")
+# Parameters
+N = X.shape[-2]  # Number of nodes in the graphs
+F = X.shape[-1]  # Node features dimensionality
+S = E.shape[-1]  # Edge features dimensionality
+n_out = 2  # Dimensionality of the target
+learning_rate = 1e-3  # Learning rate for SGD
+epochs = 25  # Number of training epochs
+batch_size = 8  # Batch size
+es_patience = 5  # Patience fot early stopping
+
+# Train/test split
+A_train, A_test, \
+X_train, X_test, \
+E_train, E_test, \
+y_train, y_test = train_test_split(A, X, E, y, test_size=0.1)
+
 # Model definition
 X_in = Input(shape=(N, F))
 A_in = Input(shape=(N, N))
@@ -40,15 +58,11 @@ E_in = Input(shape=(N, N, S))
 gc1 = EdgeConditionedConv(16, activation='relu')([X_in, A_in, E_in])
 gc2 = EdgeConditionedConv(16, activation='relu')([gc1, A_in, E_in])
 pool = GlobalAvgPool()(gc2)
-# dense1 = Dense(64)(pool)
-# dropout1 = Dropout(0.5)(dense1)
 output = Dense(n_out)(pool)
-
-from keras.optimizers import SGD
 
 # Build model
 model = Model(inputs=[X_in, A_in, E_in], outputs=output)
-model.compile(optimizer=Adam(lr=0.1, clipvalue=0.5), loss='sparse_categorical_crossentropy')
+model.compile(optimizer=Adam(lr=.00004, clipnorm=1.), loss='sparse_categorical_crossentropy')
 model.summary()
 
 checkpoint = ModelCheckpoint('tego-' + str(int(time.time())) + '.h5', monitor='val_loss', verbose=1,
