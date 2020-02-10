df5fb019036938af41b409645a32934df8b26a56
Frank Hui <huifrank@live.com>
2020-02-09 17:09:35 -0800

    Print number of data

--- tego/util.py
+++ tego/util.py
@@ -193,6 +193,7 @@ def cross_validate(A_train, X_train, E_train, y_train, A_test, X_test, E_test, y
     batch_size = 8  # Batch size
     k_folds = 5
     i = 0
+    print("{} points of data.".format(N))
     while i < N:
         # Model definition
         X_in = Input(shape=(N, F))
